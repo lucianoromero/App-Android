38e32bf3b195e5d855db2a0f85c58b4a5d406f07
Romero <luciano.luiz.romero@accenture.com>
2021-06-08 18:20:02 -0300

    Ultilizando AdapterViews para colecao de views

--- DEV/app/src/main/java/alura/com/agenda/MainActivity.java
+++ DEV/app/src/main/java/alura/com/agenda/MainActivity.java
@@ -2,16 +2,32 @@ package alura.com.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Atribuindo nosso arquivo de layout a nossa activity
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Luciano", "Ana", "Jose Carlos"));
+
+        //Pegando os campos criados no nosso layout e atribuindo valores
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
+
+
+
     }
 }
