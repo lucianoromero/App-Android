9ab54e030f6f4284bd70a69bb7d553980df6004f
Romero <luciano.luiz.romero@accenture.com>
2021-06-08 16:31:08 -0300

    Adicionando Comportamento na Activity

--- DEV/app/src/main/java/alura/com/agenda/MainActivity.java
+++ DEV/app/src/main/java/alura/com/agenda/MainActivity.java
@@ -1,6 +1,16 @@
 package alura.com.agenda;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 
 public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Luciano Teste Teste", Toast.LENGTH_LONG).show();
+    }
 }
