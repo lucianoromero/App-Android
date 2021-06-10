a8f8e938164625f7cd72fa63583c5b45bd8d72b3
Romero <luciano.luiz.romero@accenture.com>
2021-06-10 10:45:28 -0300

    Criacao de uma nova Activity e refatoração do codigo

---
+++ DEV/app/src/main/java/alura/com/agenda/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,15 @@
+package alura.com.agenda.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alura.com.agenda.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_aluno);
+    }
+}
--- DEV/app/src/main/java/alura/com/agenda/ui/activity/ListaAlunosActivity.java
+++ DEV/app/src/main/java/alura/com/agenda/ui/activity/ListaAlunosActivity.java
@@ -1,4 +1,4 @@
-package alura.com.agenda;
+package alura.com.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -11,20 +11,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import alura.com.agenda.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Atribuindo nosso arquivo de layout a nossa activity
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
         //Atribuindo titulo
         setTitle("Lista de Alunos");
 
         List<String> alunos = new ArrayList<>(Arrays.asList("Luciano", "Ana", "Jose Carlos"));
 
         //Pegando os campos criados no nosso layout e atribuindo valores
-        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
         listaDeAlunos.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos));
 
 
