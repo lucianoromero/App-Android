9e85dbc482e4206df72aca086b1df5f17a006100
Romero <luciano.luiz.romero@accenture.com>
2021-06-10 14:32:57 -0300

    Criando Alunos

--- DEV/app/src/main/java/alura/com/agenda/ui/activity/FormularioAlunoActivity.java
+++ DEV/app/src/main/java/alura/com/agenda/ui/activity/FormularioAlunoActivity.java
@@ -1,15 +1,40 @@
 package alura.com.agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import alura.com.agenda.R;
+import alura.com.agenda.model.Aluno;
 
 public class FormularioAlunoActivity extends AppCompatActivity {
+
+    private EditText campoEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_aluno);
+
+        final EditText campoNome = findViewById(R.id.activity_formulario_aluno_nome);
+        final EditText campoTelefone = findViewById(R.id.activity_formulario_aluno_telefone);
+        final EditText campoEmail = findViewById(R.id.activity_formulario_aluno_email);
+
+        Button botaoSalvar = findViewById(R.id.activity_formulario_aluno_botao_salvar);
+        //Criando a ação de salvar
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                //Criando o objeto Aluno
+                Aluno alunocriado = new Aluno(nome, telefone, email);
+            }
+        });
     }
 }
